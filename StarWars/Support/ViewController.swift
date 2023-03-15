5e8d9f6b866924d37f2382670207ce7818bdb3df
Abdelrhman <AbdlrhmanElmahdy@gmail.com>
2023-03-15 17:19:04 +0200

    Initial commit

---
+++ StarWars/Support/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Abdelrhman Elmahdy on 15/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
